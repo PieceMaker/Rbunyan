1b187abd5f258db1607ead501734ba82ef30407e
Jonathan Adams <jadams@armt.com>
2016-05-03 11:00:07 -0500

    Missed a NULL replacement for log connection.

--- R/bunyanSetLog.R
+++ R/bunyanSetLog.R
@@ -74,7 +74,7 @@ function(level, logpath, logfile, memlines, jsonout=FALSE, verbose = FALSE )  {
         flush(bunyan_globals$log_con)
         close(bunyan_globals$log_con)
       }
-      log_con <- NULL
+      log_con <- stdout()
     }
   } else {
     if(missing(logpath)) { # Use Current directory
